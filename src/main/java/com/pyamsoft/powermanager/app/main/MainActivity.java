d1542b8885e7077dc018abd57db8046980e2a6da
pyamsoft <pyam.soft@gmail.com>
2016-06-08 17:26:27 -0700

    Add theme and first main activity

---
+++ src/main/java/com/pyamsoft/powermanager/app/main/MainActivity.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2016 Peter Kenji Yamanaka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pyamsoft.powermanager.app.main;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.text.Spannable;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import com.pyamsoft.powermanager.BuildConfig;
+import com.pyamsoft.powermanager.R;
+import com.pyamsoft.pydroid.base.activity.DonationActivityBase;
+import com.pyamsoft.pydroid.support.RatingDialog;
+import com.pyamsoft.pydroid.util.StringUtil;
+
+public class MainActivity extends DonationActivityBase implements RatingDialog.ChangeLogProvider {
+
+  @Nullable @BindView(R.id.main_toolbar) Toolbar toolbar;
+  @Nullable private Unbinder unbinder;
+
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    setTheme(R.style.Theme_PowerManager_Light);
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    unbinder = ButterKnife.bind(this);
+    setupAppBar();
+  }
+
+  @Override protected void onDestroy() {
+    super.onDestroy();
+
+    assert unbinder != null;
+    unbinder.unbind();
+  }
+
+  private void setupAppBar() {
+    assert toolbar != null;
+    toolbar.setTitle(getString(R.string.app_name));
+    setSupportActionBar(toolbar);
+    setActionBarUpEnabled(false);
+  }
+
+  @NonNull @Override protected String getPlayStoreAppPackage() {
+    return getPackageName();
+  }
+
+  @NonNull @Override public Spannable getChangeLogText() {
+    // The changelog text
+    final String title = "What's New in Version " + BuildConfig.VERSION_NAME;
+    final String line1 = "BUGFIX: Code cleanup and general bugfixes";
+    final String line2 = "FEATURE: This change log screen";
+
+    // Turn it into a spannable
+    final Spannable spannable = StringUtil.createBuilder(title, "\n\n", line1, "\n\n", line2);
+
+    int start = 0;
+    int end = title.length();
+    final int largeSize =
+        StringUtil.getTextSizeFromAppearance(this, android.R.attr.textAppearanceLarge);
+    final int largeColor =
+        StringUtil.getTextColorFromAppearance(this, android.R.attr.textAppearanceLarge);
+    final int smallSize =
+        StringUtil.getTextSizeFromAppearance(this, android.R.attr.textAppearanceSmall);
+    final int smallColor =
+        StringUtil.getTextColorFromAppearance(this, android.R.attr.textAppearanceSmall);
+
+    StringUtil.boldSpan(spannable, start, end);
+    StringUtil.sizeSpan(spannable, start, end, largeSize);
+    StringUtil.colorSpan(spannable, start, end, largeColor);
+
+    start += end + 2;
+    end += 2 + line1.length() + 2 + line2.length();
+
+    StringUtil.sizeSpan(spannable, start, end, smallSize);
+    StringUtil.colorSpan(spannable, start, end, smallColor);
+
+    return spannable;
+  }
+
+  @Override public int getChangeLogIcon() {
+    return R.mipmap.ic_launcher;
+  }
+
+  @NonNull @Override public String getChangeLogPackageName() {
+    return getPackageName();
+  }
+
+  @Override public int getChangeLogVersion() {
+    return BuildConfig.VERSION_CODE;
+  }
+}
