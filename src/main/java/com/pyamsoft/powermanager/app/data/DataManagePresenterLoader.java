aa6bf9f6f12bf47a5b28349998c1b2657461b43e
pyamsoft <pyam.soft@gmail.com>
2016-10-26 00:28:36 -0700

    Update data module

--- src/main/java/com/pyamsoft/powermanager/app/data/DataManagePresenterLoader.java
+++ src/main/java/com/pyamsoft/powermanager/app/data/DataManagePresenterLoader.java
@@ -18,7 +18,7 @@ package com.pyamsoft.powermanager.app.data;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import com.pyamsoft.powermanager.PowerManager;
+import com.pyamsoft.powermanager.PowerManagerSingleInitProvider;
 import com.pyamsoft.powermanager.app.base.BaseManagePreferencePresenter;
 import com.pyamsoft.pydroid.app.PersistLoader;
 import javax.inject.Inject;
@@ -27,14 +27,15 @@ import javax.inject.Provider;
 
 public class DataManagePresenterLoader extends PersistLoader<BaseManagePreferencePresenter> {
 
-  @Inject @Named("data_manage_pref") Provider<BaseManagePreferencePresenter> presenterProvider;
+  @SuppressWarnings("WeakerAccess") @Inject @Named("data_manage_pref")
+  Provider<BaseManagePreferencePresenter> presenterProvider;
 
   DataManagePresenterLoader(@NonNull Context context) {
     super(context);
   }
 
   @NonNull @Override public BaseManagePreferencePresenter loadPersistent() {
-    PowerManager.get(getContext()).provideComponent().plusDataScreenComponent().inject(this);
+    PowerManagerSingleInitProvider.get().provideComponent().plusDataScreenComponent().inject(this);
     return presenterProvider.get();
   }
 }
