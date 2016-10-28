1e7a3415a4c652132256358f784394f2a0cc0d85
pyamsoft <pyam.soft@gmail.com>
2016-10-28 11:41:07 -0700

    Show an airplane icon in airplane mode if the toggle respects it

--- src/main/java/com/pyamsoft/powermanager/app/sync/SyncFragment.java
+++ src/main/java/com/pyamsoft/powermanager/app/sync/SyncFragment.java
@@ -41,6 +41,10 @@ public class SyncFragment extends BaseOverviewPagerFragment {
     return fragment;
   }
 
+  @Override protected boolean respectAirplaneMode() {
+    return false;
+  }
+
   @Override protected void injectObserverModifier() {
     PowerManagerSingleInitProvider.get().provideComponent().plusSyncScreenComponent().inject(this);
   }
