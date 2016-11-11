1cfeb255b9c33dd4467dd5bb058d17bb3733dd9c
pyamsoft <pyam.soft@gmail.com>
2016-11-11 11:07:49 -0800

    Format log messages with date/time and cleaner line breaking

--- src/main/java/com/pyamsoft/powermanager/dagger/logger/LoggerInteractorImpl.java
+++ src/main/java/com/pyamsoft/powermanager/dagger/logger/LoggerInteractorImpl.java
@@ -29,14 +29,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
+import java.util.StringTokenizer;
 import rx.Observable;
 import rx.exceptions.Exceptions;
 import timber.log.Timber;
 
 abstract class LoggerInteractorImpl implements LoggerInteractor {
 
+  @NonNull private static final DateFormat DATE_FORMAT = DateFormat.getDateTimeInstance();
+  private static final int MAX_LINE_LENGTH = 80;
   @SuppressWarnings("WeakerAccess") @NonNull final Context appContext;
   @SuppressWarnings("WeakerAccess") @NonNull final PowerManagerPreferences preferences;
   @SuppressWarnings("WeakerAccess") @NonNull final String logPath;
@@ -90,12 +96,40 @@ abstract class LoggerInteractorImpl implements LoggerInteractor {
           final OutputStreamWriter outputStreamWriter = new OutputStreamWriter(bufferedOutputStream,
               StandardCharsets.UTF_8);
           final BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter)) {
-        bufferedWriter.write(message);
+        final String formattedMessage = formatMessage(message);
+        bufferedWriter.write(formattedMessage);
         bufferedWriter.flush();
       } catch (IOException e) {
         throw Exceptions.propagate(e);
       }
       return true;
     });
+  }
+
+  @NonNull @CheckResult private String formatMessage(String message) {
+    final String datePrefix = DATE_FORMAT.format(Calendar.getInstance().getTime());
+    return String.format(Locale.getDefault(), "[%s] - %s", datePrefix, addLinebreaks(message));
+  }
+
+  // Bug: if a word in the input is longer than maxLineLength it will be appended to the current
+  // line instead of on a too-long line of its own. I assume line length is something like
+  // 80 or 120 characters, in which case this is unlikely to be a problem.
+  @NonNull @CheckResult private String addLinebreaks(@NonNull String input) {
+    final StringTokenizer tok = new StringTokenizer(input, " ");
+    final StringBuilder output = new StringBuilder(input.length());
+    int lineLen = 0;
+    while (tok.hasMoreTokens()) {
+      final String word = tok.nextToken();
+
+      if (lineLen + word.length() > MAX_LINE_LENGTH) {
+        output.append("\n");
+        lineLen = 0;
+      }
+
+      output.append(word);
+      lineLen += word.length();
+    }
+
+    return output.toString();
   }
 }
