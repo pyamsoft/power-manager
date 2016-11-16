8d73e7f7226d1001d53baa0b51b9614f6a71cb77
pyamsoft <pyam.soft@gmail.com>
2016-11-16 13:29:58 -0800

    Add a simple debug logger interface

--- src/main/java/com/pyamsoft/powermanager/app/logger/LoggerDialog.java
+++ src/main/java/com/pyamsoft/powermanager/app/logger/LoggerDialog.java
@@ -16,14 +16,40 @@
 
 package com.pyamsoft.powermanager.app.logger;
 
-import android.app.Dialog;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.pyamsoft.powermanager.R;
+import com.pyamsoft.powermanager.databinding.DialogLoggerBinding;
 
 public class LoggerDialog extends DialogFragment {
 
-  @NonNull @Override public Dialog onCreateDialog(Bundle savedInstanceState) {
-    return super.onCreateDialog(savedInstanceState);
+  private DialogLoggerBinding binding;
+
+  @Nullable @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    binding = DataBindingUtil.inflate(inflater, R.layout.dialog_logger, container, false);
+    return binding.getRoot();
+  }
+
+  @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    addOptionsPreferenceFragment();
+  }
+
+  private void addOptionsPreferenceFragment() {
+    final FragmentManager fragmentManager = getChildFragmentManager();
+    if (fragmentManager.findFragmentByTag(LoggerPreferenceFragment.TAG) == null) {
+      fragmentManager.beginTransaction()
+          .replace(R.id.power_trigger_preference_container, new LoggerPreferenceFragment(),
+              LoggerPreferenceFragment.TAG)
+          .commit();
+    }
   }
 }
