af2b91e07076140d52348265a1d7d0b8ded5e8ed
pyamsoft <pyam.soft@gmail.com>
2017-02-05 00:24:43 -0800

    Exit if in leak canary

--- powermanager/src/main/java/com/pyamsoft/powermanager/PowerManager.java
+++ powermanager/src/main/java/com/pyamsoft/powermanager/PowerManager.java
@@ -38,6 +38,10 @@ public class PowerManager extends PYDroidApplication {
     }
   }
 
+  @Override protected boolean exitBeforeInitialization() {
+    return LeakCanary.isInAnalyzerProcess(this);
+  }
+
   @Override protected void onCreateInDebugMode() {
     super.onCreateInDebugMode();
     refWatcher = LeakCanary.install(this);
