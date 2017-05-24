6557022fa5c75806d202bbe8b0ebbcfd231ca0f0
pyamsoft <pyam.soft@gmail.com>
2017-05-23 19:56:35 -0700

    convert main module to kotlin

---
+++ powermanager-main/src/main/java/com/pyamsoft/powermanager/main/MainInteractor.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2017 Peter Kenji Yamanaka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pyamsoft.powermanager.main
+
+import android.support.annotation.CheckResult
+import com.pyamsoft.powermanager.base.preference.RootPreferences
+import com.pyamsoft.powermanager.base.preference.ServicePreferences
+import com.pyamsoft.powermanager.model.PermissionObserver
+import io.reactivex.Single
+import javax.inject.Inject
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Singleton internal class MainInteractor @Inject constructor(val rootPreferences: RootPreferences,
+    val servicePreferences: ServicePreferences,
+    @param:Named("obs_root_permission") val rootPermissionObserver: PermissionObserver) {
+
+  /**
+   * public
+   */
+  fun missingRootPermission() {
+    rootPreferences.resetRootEnabled()
+  }
+
+  /**
+   * public
+   */
+  val isStartWhenOpen: Single<Boolean>
+    @CheckResult get() = Single.fromCallable { servicePreferences.startWhenOpen }
+
+  /**
+   * public
+   */
+  @CheckResult fun hasRootPermission(): Single<Boolean> {
+    return Single.fromCallable { rootPermissionObserver.hasPermission() }
+  }
+}
---
+++ powermanager-main/src/main/java/com/pyamsoft/powermanager/main/MainPresenter.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2017 Peter Kenji Yamanaka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pyamsoft.powermanager.main
+
+import com.pyamsoft.pydroid.presenter.SchedulerPresenter
+import io.reactivex.Scheduler
+import javax.inject.Inject
+import javax.inject.Named
+import timber.log.Timber
+
+internal class MainPresenter @Inject constructor(val interactor: MainInteractor,
+    @Named("obs") obsScheduler: Scheduler,
+    @Named("sub") subScheduler: Scheduler) : SchedulerPresenter(obsScheduler, subScheduler) {
+
+  /**
+   * public
+   */
+  fun runStartupHooks(callback: StartupCallback) {
+    startServiceWhenOpen(callback)
+    checkForRoot(callback)
+  }
+
+  private fun startServiceWhenOpen(callback: StartupCallback) {
+    disposeOnStop(interactor.isStartWhenOpen
+        .subscribeOn(subscribeScheduler)
+        .observeOn(observeScheduler)
+        .subscribe({ start ->
+          if (start!!) {
+            callback.onServiceEnabledWhenOpen()
+          }
+        }) { throwable -> Timber.e(throwable, "onError isStartWhenOpen") })
+  }
+
+  private fun checkForRoot(callback: StartupCallback) {
+    disposeOnStop(interactor.hasRootPermission()
+        .subscribeOn(subscribeScheduler)
+        .observeOn(observeScheduler)
+        .subscribe({ hasPermission ->
+          if (!hasPermission) {
+            interactor.missingRootPermission()
+            callback.explainRootRequirement()
+          }
+        }) { throwable -> Timber.e(throwable, "onError checking root permission") })
+  }
+
+  internal interface StartupCallback {
+
+    fun onServiceEnabledWhenOpen()
+
+    fun explainRootRequirement()
+  }
+}
